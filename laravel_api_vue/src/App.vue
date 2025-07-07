82970aefd61d5307f8ae3aa0d5528950f4cab47f
Kaan Kaltakkıran <durdu.kaan.kaltakkiran@gmail.com>
2025-07-07 10:53:09 +0300

    laravel api with vue proje created

---
+++ laravel_api_vue/src/App.vue
@@ -0,0 +1,15 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+  <header>
+    <nav>
+      <RouterLink class="nav-link" to="/">Home</RouterLink>
+    </nav>
+  </header>
+
+  <RouterView />
+</template>
+
+<style scoped></style>
