0e601a9d1e5de559743d771bded857594b4af65f
Kaan Kaltakkıran <durdu.kaan.kaltakkiran@gmail.com>
2025-07-11 00:10:09 +0300

    created auth controller set register login and logout with router api

--- laravel-todo-api/routes/api.php
+++ laravel-todo-api/routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\TodoController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -9,3 +10,6 @@ Route::get('/user', function (Request $request) {
 })->middleware('auth:sanctum');
 
 Route::apiResource('todos', TodoController::class);
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
