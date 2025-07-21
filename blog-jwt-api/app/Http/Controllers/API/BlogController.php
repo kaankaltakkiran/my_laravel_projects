73b35d0665a869aee611044592bed09eaa74a549
Kaan Kaltakkıran <durdu.kaan.kaltakkiran@gmail.com>
2025-07-21 11:29:49 +0300

    set and created blog controller

---
+++ blog-jwt-api/app/Http/Controllers/API/BlogController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use App\Models\Blog;
+use Illuminate\Http\Request;
+
+class BlogController extends Controller
+{
+
+    public function index()
+    {
+
+        return Blog::all();
+    }
+    public function store(Request $request)
+    {
+        $blog = Blog::create($request->all());
+        return $blog;
+    }
+    public function show(Blog $blog)
+    {
+        return $blog;
+    }
+    public function update(Request $request, Blog $blog)
+    {
+        $blog->update($request->all());
+        return $blog;
+    }
+
+    public function destroy(Blog $blog)
+    {
+
+        $blog->delete();
+        return ["message" => "Blog silindi"];
+    }
+}
