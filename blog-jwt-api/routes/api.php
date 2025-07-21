53ed89c8bbadd65906d44b6c0ad06d40d6e3d1ff
Kaan Kaltakkıran <durdu.kaan.kaltakkiran@gmail.com>
2025-07-21 14:44:35 +0300

    update api php

--- blog-jwt-api/routes/api.php
+++ blog-jwt-api/routes/api.php
@@ -1,5 +1,19 @@
 <?php
+use App\Http\Controllers\API\AuthController;
 use App\Http\Controllers\API\BlogController;
 use Illuminate\Support\Facades\Route;
 
 Route::apiResource('blogs', BlogController::class);
+
+// JWT Token olması gereken işlemler
+Route::middleware('auth:api')->group(function () {
+    Route::post('logout', [AuthController::class, 'logout']);
+    Route::get('me', [AuthController::class, 'me']);
+    Route::post('refresh', [AuthController::class, 'refresh']);
+});
+
+// JWT Token olmayan işlemler
+Route::controller(AuthController::class)->group(function () {
+    Route::post('register', [AuthController::class, 'register']);
+    Route::post('login', [AuthController::class, 'login']);
+});
