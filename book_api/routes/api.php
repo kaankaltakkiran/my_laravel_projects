b53348077a972f93138450916dd4f894226a13d3
Kaan Kaltakkıran <durdu.kaan.kaltakkiran@gmail.com>
2025-07-25 15:10:44 +0300

    update api route

--- book_api/routes/api.php
+++ book_api/routes/api.php
@@ -1,10 +1,23 @@
 <?php
-
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\AuthorController;
 use App\Http\Controllers\BookController;
 use App\Http\Controllers\CategoryController;
 use Illuminate\Support\Facades\Route;
 
-Route::apiResource('authors', AuthorController::class);
-Route::apiResource('categories', CategoryController::class);
-Route::apiResource('books', BookController::class);
+// JWT Token olması gereken işlemler
+Route::middleware('auth:api')->group(function () {
+    Route::post('logout', [AuthController::class, 'logout']);
+    Route::get('profile', [AuthController::class, 'profile']);
+    Route::post('refresh', [AuthController::class, 'refresh']);
+
+    Route::apiResource('authors', AuthorController::class);
+    Route::apiResource('categories', CategoryController::class);
+    Route::apiResource('books', BookController::class);
+});
+
+// JWT Token olmayan işlemler
+Route::controller(AuthController::class)->group(function () {
+    Route::post('register', [AuthController::class, 'register']);
+    Route::post('login', [AuthController::class, 'login']);
+});
