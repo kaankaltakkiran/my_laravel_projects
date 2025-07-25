0d80a9f0d9b7615233140a577a0248281e37205a
Kaan Kaltakkıran <durdu.kaan.kaltakkiran@gmail.com>
2025-07-25 15:10:18 +0300

    set user model and auth controller

---
+++ book_api/app/Http/Controllers/AuthController.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+
+class AuthController extends Controller
+{
+    public function __construct()
+    {
+        $this->middleware('auth:api', ['except' => ['login', 'register']]);
+    }
+
+    public function register(Request $request)
+    {
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+        ]);
+
+        return response()->json([
+            'message' => 'Kullanıcı başarıyla kaydedildi',
+            'user' => $user,
+        ]);
+    }
+
+    public function login(Request $request)
+    {
+        $credentials = $request->only('email', 'password');
+
+        $token = Auth::attempt($credentials);
+
+        if (!$token) {
+            return response()->json(['error' => 'Email veya şifre yanlış'], 401);
+        }
+
+        $user = Auth::user();
+        return response()->json([
+            'message' => 'Giriş başarılı',
+            'user' => $user,
+            'authorisation' => [
+                'token' => $token,
+                'type' => 'bearer',
+            ],
+        ]);
+    }
+
+    public function profile()
+    {
+        return response()->json(Auth::user());
+    }
+
+    public function logout()
+    {
+        Auth::logout();
+        return response()->json(['message' => 'Çıkış başarılı.']);
+    }
+
+    public function refresh()
+    {
+        $user = Auth::user();
+        $token = Auth::refresh();
+        return response()->json([
+            'message' => 'Yenileme başarılı',
+            'user' => $user,
+            'authorisation' => [
+                'token' => $token,
+                'type' => 'bearer',
+            ],
+        ]);
+    }
+}
