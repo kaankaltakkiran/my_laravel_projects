67b39103d0cc02e15c0f53006741d72d9b5bd1cd
Kaan Kaltakkıran <durdu.kaan.kaltakkiran@gmail.com>
2025-08-03 21:31:42 +0300

    create listener and event and set provider

---
+++ laravel_api_jwt_telegram/app/Providers/EventServiceProvider.php
@@ -0,0 +1,30 @@
+<?php
+namespace App\Providers;
+
+use App\Events\UserRegistered;
+use App\Listeners\SendTelegramNotification;
+use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
+
+class EventServiceProvider extends ServiceProvider
+{
+    /**
+     * The event listener mappings for the application.
+     *
+     * @var array
+     */
+    protected $listen = [
+        UserRegistered::class => [
+            SendTelegramNotification::class,
+        ],
+    ];
+
+    /**
+     * Register any events for your application.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        parent::boot();
+    }
+}
